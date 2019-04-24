a75bb153d5a862e4b1792202ba1aaa760d68f341
Andre Torres <andre.torres@codurance.com>
2019-04-24 20:10:23 +0100

    Refactor javascript to make more functional

--- js/codurance.js
+++ js/codurance.js
@@ -2,23 +2,26 @@ const rootEl = document.documentElement;
 
 const modalButtons = () => document.querySelectorAll("[data-toggle]");
 
+const isCloseButton = (event) => event.target.attributes['aria-label'].value === 'close';
+
+const openModal = modal => event => {
+    rootEl.classList.add('is-clipped');
+    modal.classList.add('is-active');
+}
+
+const closeModal = modal => event => {
+    if (isCloseButton(event)) {
+        modal.classList.remove('is-active');
+        rootEl.classList.add('is-clipped');
+    }
+}
+
 const addModalListener = (element) => {
     const target = element.attributes['data-target'].value;
     const modal = document.getElementById(target);
 
-    element.addEventListener('click', (event) => {
-        rootEl.classList.add('is-clipped');
-        modal.classList.add('is-active');
-    });
-    
-    modal.addEventListener('click', (event) => {
-        const ariaLabel = event.target.attributes['aria-label'].value;
-
-        if (ariaLabel === 'close') {
-            modal.classList.remove('is-active');
-            rootEl.classList.add('is-clipped');
-        }          
-    });
+    element.addEventListener('click', openModal(modal));
+    modal.addEventListener('click', closeModal(modal));
 };
 
 modalButtons().forEach(addModalListener);
