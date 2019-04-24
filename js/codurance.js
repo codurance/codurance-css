665bbef206eee322a35e6552bad41d3707eacf16
Andre Torres <andre.torres@codurance.com>
2019-04-24 17:49:29 +0100

    Add javascript for modal

---
+++ js/codurance.js
@@ -0,0 +1,38 @@
+const rootEl = document.documentElement;
+
+const modalButtons = () => document.querySelectorAll("[data-toggle]");
+
+const openModal = (modal, event) => {
+    rootEl.classList.add('is-clipped');
+    modal.classList.add('is-active');
+}
+
+const closeModal = (modal, event) => {
+    const ariaLabel = event.target.attributes['aria-label'].value;
+
+    if (ariaLabel === 'close') {
+        modal.classList.remove('is-active');
+        rootEl.classList.add('is-clipped');
+    }
+}
+
+const addModalListener = (element) => {
+    const target = element.attributes['data-target'].value;
+    const modal = document.getElementById(target);
+
+    element.addEventListener('click', (event) => {
+        rootEl.classList.add('is-clipped');
+        modal.classList.add('is-active');
+    });
+    
+    modal.addEventListener('click', (event) => {
+        const ariaLabel = event.target.attributes['aria-label'].value;
+
+        if (ariaLabel === 'close') {
+            modal.classList.remove('is-active');
+            rootEl.classList.add('is-clipped');
+        }          
+    });
+};
+
+modalButtons().forEach(addModalListener);
