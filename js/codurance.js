6d1efc10184e756787774b81db643319baa62c6d
Andre Torres <andre.torres@codurance.com>
2019-04-24 17:58:06 +0100

    remove unused code

--- js/codurance.js
+++ js/codurance.js
@@ -2,20 +2,6 @@ const rootEl = document.documentElement;
 
 const modalButtons = () => document.querySelectorAll("[data-toggle]");
 
-const openModal = (modal, event) => {
-    rootEl.classList.add('is-clipped');
-    modal.classList.add('is-active');
-}
-
-const closeModal = (modal, event) => {
-    const ariaLabel = event.target.attributes['aria-label'].value;
-
-    if (ariaLabel === 'close') {
-        modal.classList.remove('is-active');
-        rootEl.classList.add('is-clipped');
-    }
-}
-
 const addModalListener = (element) => {
     const target = element.attributes['data-target'].value;
     const modal = document.getElementById(target);
