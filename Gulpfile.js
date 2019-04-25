0c230a90f3c9febbcaf17197326c145cb6fb268f
Andre Torres <andre.torres@codurance.com>
2019-04-25 13:45:31 +0100

    Add task to minify css and copy new css to docs

--- Gulpfile.js
+++ Gulpfile.js
@@ -9,10 +9,20 @@ const compileCss = () => {
         .pipe(gulp.dest('./dist/css'));
 };
 
+const minifyCss = () => {
+    return gulp.src('./sass/**/*.scss')
+        .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+        .pipe(rename({suffix: '.min'}))
+        .pipe(gulp.dest('./dist/css'));
+};
+
+const updateDocsCSS = () => {
+    return gulp.src('./dist/css/codurance.css')            
+            .pipe(gulp.dest('./docs/assets/css'));
+}
 
 gulp.task('sass', gulp.series(compileCss, updateDocsCSS));
 gulp.task('minify', minifyCss)
-
 
 gulp.task('sass:watch', function () {
     gulp.watch('./sass/**/*.scss', compileCss);
